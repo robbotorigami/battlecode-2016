f7aeb31c52451dcfb2460dfaa395389964e91ce7
robbotorigami <robbotorigami@gmail.com>
2016-01-05 10:18:35 -0700

    Merged combat micro

--- Soldier.java
+++ Soldier.java
@@ -11,11 +11,35 @@ public class Soldier extends BaseRobot {
 	@Override
 	public void run() {
 		while(true){
-			try {
-				kiteingMicro();
-			} catch (GameActionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			RobotInfo[] bots = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, rc.getTeam());
+			boolean neededToMove = false;
+			for(RobotInfo bot : bots){
+				if(bot.type == RobotType.ARCHON){
+					if(rc.getLocation().distanceSquaredTo(bot.location) < 3){
+						if(rc.canMove(rc.getLocation().directionTo(bot.location).opposite())){
+							neededToMove = true;
+							if(rc.isCoreReady()){
+								try {
+									rc.move(rc.getLocation().directionTo(bot.location).opposite());
+								} catch (GameActionException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							}
+						}
+					}
+				}
+			}
+			if(!neededToMove){
+				rc.setIndicatorString(0, "In Position, TIME TO KILL!!!");
+				try {
+					destroy();
+				} catch (GameActionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else{
+				rc.setIndicatorString(0, "Not in position");
 			}
 			Clock.yield();
 		}
