558f0d3ad1690539cd26fbfcfc1a8b26256cce50
BuildTools <unconfigured@null.spigotmc.org>
2016-01-06 20:01:31 -0700

    OGIUSDFJBKFSD

---
+++ Guard.java
@@ -0,0 +1,18 @@
+package team022;
+
+import battlecode.common.RobotController;
+
+public class Guard extends BaseRobot {
+
+	public Guard(RobotController rcin) {
+		super(rcin);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
